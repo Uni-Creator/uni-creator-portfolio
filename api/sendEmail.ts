7c063f91a166cd6a519a5a67ca11ce9233eb35f1
shivam <shivam99singh33@gmail.com>
2025-09-13 21:12:23 +0530

    fixed type error of form

--- api/sendEmail.ts
+++ api/sendEmail.ts
@@ -1,8 +1,11 @@
 import emailjs from "@emailjs/browser";
 import {contactDetails} from "../constants"
 
-export const sendEmail = async (form: React.RefObject<HTMLFormElement>) => {
+export const sendEmail = async (form: React.RefObject<HTMLFormElement | null>) => {
   try {
+    if(!form.current) {
+      throw new Error("Form error")
+    }
     const result = await emailjs.send(
       import.meta.env.VITE_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
