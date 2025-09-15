9fdca4408b608285bf1edbff247ad4e65de6ceef
Abhay Singh <73036098+Uni-Creator@users.noreply.github.com>
2025-09-15 15:45:31 +0530

    Update aboutMe.ts

--- constants/aboutMe.ts
+++ constants/aboutMe.ts
@@ -1,18 +1,21 @@
 import type { AboutProps } from "./constantTtypes";
 const aboutData: AboutProps = {
   heading: "About Me",
-  description:
-    "Hi! I’m Abhay Singh 👩‍💻, an AI/ML Engineer with a strong foundation in mathematics and cybersecurity. Passionate about deep learning, computer vision, and NLP, I’ve worked on projects ranging from real-time sign language recognition to AI-powered SaaS applications.",
-  highlights: [
+ description:
+    "I am an engineering student specializing in artificial intelligence, machine learning, and computer systems. I focus on building efficient, data-driven solutions through strong programming, analytical, and problem-solving skills. With experience in AI/ML, software development, and research, I am driven to create innovative technologies that bridge theory and real-world applications.",
+   highlights: [
     { text: "Abhay Singh"},
-    { text: "AI/ML Engineer"},
-    { text: "mathematics"},
-    { text: "cybersecurity"},
-    { text: "deep learning"},
-    { text: "computer vision"},
-    { text: "NLP"},
-    { text: "real-time sign language recognition"},
-    { text: "SaaS"},
+   { text: "artificial intelligence" },
+    { text: "machine learning" },
+    { text: "computer systems" },
+    { text: "data-driven solutions" },
+    { text: "programming" },
+    { text: "analytical" },
+    { text: "problem-solving" },
+    { text: "AI/ML" },
+    { text: "software development" },
+    { text: "research" },
+    { text: "innovative technologies" },
   ],
   tagline: "Let’s connect and innovate together!",
 };
